97d8608531b81f7cd2a3ba82f468e5d06e787b3a
LoiNT <ntlzz5793@hotmail.com>
2016-05-26 15:20:26 +0700

    first commit

---
+++ MeCung/GameSettings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeCung
+{
+    public class treeSearch
+    {
+        public int position {get;set;}
+        public int cost { get; set; }
+        public int distance { get; set; }
+
+        public List<int> elements;
+        public treeSearch link1;
+        public treeSearch link2;
+        public treeSearch link3;
+        public treeSearch link4;
+    }
+
+    class GameSettings
+    {
+        public List<int> board;
+        public int getSize()
+        {
+            return board.Count;
+        }
+        public GameSettings()
+        {
+
+        }
+
+        public void startGame()
+        {
+
+        }
+        public void createBoard(int row,int column)
+        {
+            
+            Random rdm = new Random();
+            board = new List<int>();
+
+            for (int i = 0; i < row * column; i++)
+            {
+                int num = rdm.Next(0, 2);
+                board.Add(num);
+            }
+            int start = rdm.Next(0, 25);
+            int finish = rdm.Next(0, 25);
+            if (start != finish)
+            {
+                board[start] = 2;
+                board[finish] = 3;
+            }
+        }
+
+    }
+}
---
+++ MeCung/frmMainGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MeCung
+{
+    public partial class frmMainGame : Form
+    {
+        public frmMainGame()
+        {
+            game = new GameSettings();
+            game.createBoard(5,5);
+            Form_Load();
+        }
+
+        GameSettings game;
+        private Panel[,] _chessBoardPanels;
+
+        // event handler of Form Load... init things here
+        private void Form_Load()
+        {
+            const int tileSize = 100;
+            const int gridSize = 5;
+            var clr1 = Color.DarkGray;
+            var clr2 = Color.LightSeaGreen;
+            var clr3 = Color.DarkBlue;
+            var clr4 = Color.DarkRed;
+            int i = 0;
+
+            _chessBoardPanels = new Panel[gridSize, gridSize];
+
+            for (var n = 0; n < gridSize; n++)
+            {
+                for (var m = 0; m < gridSize; m++)
+                {
+                     var newPanel = new Panel
+                    {
+                        Size = new Size(tileSize, tileSize),
+                        Location = new Point(tileSize * n, tileSize * m)
+                    };
+
+                    Controls.Add(newPanel);
+
+                    _chessBoardPanels[n, m] = newPanel;
+
+                    if (i != game.getSize())
+                    {
+                        if (game.board[i] == 0)
+                        {
+                            newPanel.BackColor = clr1;
+                        }
+                        else if (game.board[i] == 1)
+                        {
+                            newPanel.BackColor = clr2;
+                        }
+                        else if (game.board[i] == 2)
+                        {
+                            newPanel.BackColor = clr3;
+                        }
+                        else if (game.board[i] == 3)
+                        {
+                            newPanel.BackColor = clr4;
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
